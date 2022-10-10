d6c61073c466774e181d675c4e6adcdca1a3ce59
Carlos Portes <carlosportes.c@gmail.com>
2022-10-10 11:41:10 -0400

    Update models.py

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ class DataCapture():
         self.number_list.append(number)
 
     def build_stats(self):
-        # n*Log(n)
+        # Pass sorted list to Stats class to optimize searching
         self.number_list.sort()
         return Stats(self.number_list)
 
@@ -16,6 +16,7 @@ class Stats():
 
     def less(self, number):
         counter = 0
+        # Loop through the list as long as counter < list len and searched number is less than current number on list
         while counter < len(self.sorted_number_list) and number > self.sorted_number_list[counter]:
             counter += 1
         return counter
@@ -33,5 +34,6 @@ class Stats():
             if i >= start_number and i <= end_number:
                 counter += 1
             if i > end_number:
+                # Break to avoid looping through the list needlesly
                 break
         return counter
